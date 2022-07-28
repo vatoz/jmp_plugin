d3e8b1b4b10339ae47dcc88ab56409ad19b87f2f
Vaclav Cerny <vasek@vasekcerny.cz>
2022-07-28 16:23:04 +0200

    Big rename

--- controllers/StatsController.php
+++ controllers/StatsController.php
@@ -5,114 +5,135 @@
  * ----------------------------------------------------------------------
  */
 
- 	require_once(__CA_LIB_DIR__.'/Configuration.php');
-  //require_once(__CA_MODELS_DIR__.'/ca_objects.php');
-  //require_once(__CA_MODELS_DIR__.'/ca_collections.php');
-  //require_once(__CA_MODELS_DIR__.'/ca_list_items.php');
- 	//include_once(__CA_LIB_DIR__."/Vimeo/vimeo.php");
+require_once(__CA_LIB_DIR__ . '/Configuration.php');
+//require_once(__CA_MODELS_DIR__.'/ca_objects.php');
+//require_once(__CA_MODELS_DIR__.'/ca_collections.php');
+//require_once(__CA_MODELS_DIR__.'/ca_list_items.php');
+//include_once(__CA_LIB_DIR__."/Vimeo/vimeo.php");
 
- 	class StatsController extends ActionController {
- 		# -------------------------------------------------------
- 		protected $opo_config;		// plugin configuration file
- 		# -------------------------------------------------------
- 		#
- 		# -------------------------------------------------------
- 		public function __construct(&$po_request, &$po_response, $pa_view_paths=null) {
- 			parent::__construct($po_request, $po_response, $pa_view_paths);
- 			//$this->opo_config = Configuration::load(__CA_APP_DIR__.'/conf/search.conf');
-      // Load plugin stylesheet
- 		    
-      
- 		}
- 		# -------------------------------------------------------
- 		 	
-    
-    function About(){
-      if (!$this->request->user->canDoAction('jmp_stats')) {
-          $this->response->setRedirect($this->request->config->get('error_display_url').'/n/3000?r='.urlencode($this->request->getFullUrlPath()));
-        return;
-      }
-      $this->render('stats.php');
+class StatsController extends ActionController
+{
+  # -------------------------------------------------------
+  protected $opo_config;    // plugin configuration file
+  # -------------------------------------------------------
+  #
+  # -------------------------------------------------------
+  public function __construct(&$po_request, &$po_response, $pa_view_paths = null)
+  {
+    parent::__construct($po_request, $po_response, $pa_view_paths);
+    //$this->opo_config = Configuration::load(__CA_APP_DIR__.'/conf/search.conf');
+    // Load plugin stylesheet
+  }
+  # -------------------------------------------------------
+
+
+  private function renderStat($i)
+  {
+    if (!$this->request->user->canDoAction('jmp_stats')) {
+      $this->response->setRedirect($this->request->config->get('error_display_url') . '/n/3000?r=' . urlencode($this->request->getFullUrlPath()));
+      return;
     }
-    
-    private function renderStat($i){
-      if (!$this->request->user->canDoAction('jmp_stats')) {
-          $this->response->setRedirect($this->request->config->get('error_display_url').'/n/3000?r='.urlencode($this->request->getFullUrlPath()));
-          return;
-      }
-      $this->render('stats'.$i.'.php');                                
-    }
-    
-    function RelationshipTypesCsv(){    
-      return $this->renderStat(1);                    
-    }
-    
-    function InterstitialAttribute(){
-      return $this->renderStat(2);               
-    }
-    function InterstitialIdno(){
-      return $this->renderStat(3);  
-    }
-    function UserEditors(){
-      return $this->renderStat(4);  
-    }
-    function ElementsRequired(){
-      return $this->renderStat(5);  
-    }
-    
-    function UserPrivs(){
-      return $this->renderStat(6);  
-    }
-    
-    
-    function UserCopy(){
-      return $this->renderStat(12);  
-    }
-    
-    
-    function ListItems(){
-      return $this->renderStat(7);  
-    }
-    
-    function ListItemsSex(){
-      return $this->renderStat(9);  
-    }
-    
-    function ImportImages(){
-      return $this->renderStat(10);  
-    }
-    function Regexp(){
-      return $this->renderStat(11);  
-    }
- 		   
-    function ThumbTester(){
-      return $this->renderStat(13);  
-    }
-    function MultifileTester(){
-      return $this->renderStat(14);  
-    }
-    function DRPSObjects(){
-      return $this->renderStat(15);  
-    }
-    function MultipartIdnoSequences(){
-      return $this->renderStat(16);  
-    }
-    function Places(){
-      return $this->renderStat(17);  
-    }
-    function Fonts(){
-      return $this->renderStat(18);  
-    }
-    function Map(){
-      return $this->renderStat(19);  
-    }
-    
-    function IdnoReindex(){
-      return $this->renderStat(20);  
-    }
+    $this->render('stats' . $i . '.php');
+  }
+
+  function About()
+  {
+    return $this->renderStat('');
+  }
+  function DRPSObjects()
+  {
+    return $this->renderStat('DRPSObjects');
+  }
  
+  function ElementsRequired()
+  {
+    return $this->renderStat('ElementsRequired');
+  }
+
+  function Fonts()
+  {
+    return $this->renderStat('Fonts');
+  }
+
+  function IdnoSearchRebuild()
+  {
+    return $this->renderStat('IdnoSearchRebuild');
+  }
+  function IdnoRegexp()
+  {
+    return $this->renderStat('IdnoRegexp');
+  }
+  function IdnoSequences()
+  {
+    return $this->renderStat('IdnoSequences');
+  }
+
+  function ImageThumbTester()
+  {
+    return $this->renderStat('ImageThumbTester');
+  }
+
+  function ImageMultifileTester()
+  {
+    return $this->renderStat('ImageMultifileTester');
+  }
+ 
+  function ImagesImport()
+  {
+    return $this->renderStat('ImagesImport');
+  }
+ 
+  function InterstitialAttribute()
+  {
+    return $this->renderStat('InterstitialAttribute');
+  }
+  function InterstitialIdno()
+  {
+    return $this->renderStat('InterstitialIdno');
+  }
+
+  function ListItems()
+  {
+    return $this->renderStat('ListItems');
+  
+  }
+
+  function ListItemsSex()
+  {
+    return $this->renderStat('ListItemsSex');
+  }
+
+  function PlacesMap()
+  {
+    return $this->renderStat('PlacesMap');
+  }
+  function PlacesCSV()
+  {
+    return $this->renderStat('PlacesCSV');
+  }
+
+
+  function RelationshipTypesCsv()
+  {
+    return $this->renderStat('RelationshipTypesCsv');
+  }
+
+  function UserCopy()
+  {
+    return $this->renderStat('UserCopy');
+  }
+
+  function UserEditors()
+  {
+    return $this->renderStat('UserEditors');
+  }
+
+  function UserPrivs()
+  {
+    return $this->renderStat('UserPrivs');
+  }
+
  
 
- 		
- 	}
- 
+
+}
--- views/stats.php
+++ views/stats.php
@@ -4,28 +4,35 @@
 <li><A href="/jmp/Stats/About">Odkaz na seznam statistik</a></li>
 
   
+ 
   
-<li><A href="/jmp/Stats/RelationshipTypesCsv">CSV s vztahy a kolikrát jsou použity</a></li>
-<li><A href="/jmp/Stats/InterstitialAttribute">CSV, kolikrát je interstitial attribute použit</a></li>    
-<li><A href="/jmp/Stats/InterstitialIdno">CSV, IDNO užívající interstitial </a></li>    
+
+
+<li><A href="/jmp/Stats/DRPSObjects?set=1&about=about">Kopie sady objektů do DRPS </A></li>
+
 <li><A href="/jmp/Stats/ElementsRequired">Které elementy jsou vyžadovány??? asi. </a></li>    
 
+<li><A href="/jmp/Stats/Fonts">fonty</A></li>
+
+<li><A href="/jmp/Stats/IdnoRegexp">regexp idno </a></li>    
+<li><A href="/jmp/Stats/IdnoReindex">REindexace idno</A></li>
+<li><A href="/jmp/Stats/IdnoSequences">Test na duplicitní idno</A></li>
+
+<li><A href="/jmp/Stats/ImagesImport">Kolik bylo kdy naimportováno reprezentací </a></li>    
+<li><A href="/jmp/Stats/ImageThumbTester">Kontrola IIF obrázků</A></li>
+
+<li><A href="/jmp/Stats/InterstitialAttribute">CSV, kolikrát je interstitial attribute použit</a></li>    
+<li><A href="/jmp/Stats/InterstitialIdno">CSV, IDNO užívající interstitial </a></li>    
+
 <li><A href="/jmp/Stats/ListItems">Seznamy a položky</a></li>      
-  <li><A href="/jmp/Stats/ListItemsSex">Seznamy a položky - pohlaví</a></li>      
-<li><A href="/jmp/Stats/UserPrivs">Uživatélé, role, skupiny</a></li>      
-  <li><A href="/jmp/Stats/UserCopy?from=100&to=1000">Kopírování práv uživatele</a></li>      
-  
+<li><A href="/jmp/Stats/ListItemsSex">Seznamy a položky - pohlaví</a></li>      
+
+<li><A href="/jmp/Stats/PlacesCSV">Výpis míst v čr</A></li>
+<li><A href="/jmp/Stats/PlacesMap">Mapa míst s geocoord</A></li>
+
+<li><A href="/jmp/Stats/RelationshipTypesCsv">CSV s vztahy a kolikrát jsou použity</a></li>
+
+<li><A href="/jmp/Stats/UserCopy?from=100&to=1000">Kopírování práv uživatele</a></li>      
 <li><A href="/jmp/Stats/UserEditors">Který uživatel používá které editory </a></li>    
-  
-<li><A href="/jmp/Stats/ImportImages">Kolik bylo kdy naimportováno reprezentací </a></li>    
-  <li><A href="/jmp/Stats/Regexp">Kolik bylo kdy naimportováno reprezentací </a></li>    
-  <li><A href="/jmp/Stats/ThumbTester">Kontrola IIF obrázků</A></li>
-  <li><A href="/jmp/Stats/DRPSObjects?set=1&about=about">Kopie sady objektů do DRPS </A></li>
-  <li><A href="/jmp/Stats/MultipartIdnoSequences">Test na duplicitní idno</A></li>
-
-  <li><A href="/jmp/Stats/Places">Výpis míst v čr</A></li>
-  <li><A href="/jmp/Stats/Fonts">fonty</A></li>
-  <li><A href="/jmp/Stats/Map">Mapa</A></li>
-
-</ul>  
-  
+<li><A href="/jmp/Stats/UserPrivs">Uživatélé, role, skupiny</a></li>      
+</ul> 
