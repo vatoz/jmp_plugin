57d72d2340436637976bc5d9c3bdef2f323da806
Vaclav Cerny <vasek@vasekcerny.cz>
2022-08-04 15:20:28 +0200

    lepší

--- views/statsObjectsPrimaryImageHidden.php
+++ views/statsObjectsPrimaryImageHidden.php
@@ -19,14 +19,16 @@ try {
 //megahaluz, několikrát vnořený dotaz co získá seznam objektů, které mají primární obrázek neviditelný, ale mají nějaký, který je omezený nebo veřejný
 
   $SQL= "
-  select  ca_objects.object_id, ca_objects.idno,  min(ca_objects_x_object_representations.representation_id) as obrazek from 
+  select  ca_objects.object_id, ca_objects.idno,  ca_objects_x_object_representations.representation_id, ca_object_representations.access
+  
+  from 
 ca_objects_x_object_representations
 left join  ca_object_representations 
 on ca_objects_x_object_representations.representation_id 
 =  ca_object_representations.representation_id 
 left join ca_objects on ca_objects_x_object_representations.object_id=ca_objects.object_id
 
-where ca_object_representations.access<>1435 and
+where 
 ca_objects.access<>1435
  and ca_objects_x_object_representations.object_id in (
 
@@ -43,7 +45,7 @@ SELECT representation_id
 FROM `ca_objects_x_object_representations` where is_primary=1) and
 access=1435))
 
-group by object_id having count(*) >1) group by object_id
+group by object_id having count(*) >1) order by  ca_objects.object_id
   
   ";
 
@@ -52,21 +54,26 @@ group by object_id having count(*) >1) group by object_id
   $cnt=0;
   $dotaz = $pdo->query($SQL);
   $nok=array();
-  
+  $last="";
   foreach($dotaz  as $Row){
 
+    if($Row["idno"]!==$last){
+      echo "<hr> <a href='/index.php/editor/objects/ObjectEditor/Edit/object_id/".
+      $Row["object_id"]."'>". $Row["idno"]."</a>";
+      $last=$Row["idno"];
+    }
 
 
-    echo "<hr> <a href='/index.php/editor/objects/ObjectEditor/Edit/object_id/".
-    $Row["object_id"]."'>". $Row["idno"]."</a>";
 
-
-    $t_media = new ca_object_representations( $Row["obrazek"]);
-    $m=$r->getRepresentationMediaForIDs(array( $Row["obrazek"]),array("preview", "small", "medium", "large")  );
+    $t_media = new ca_object_representations( $Row["representation_id"]);
+    $m=$r->getRepresentationMediaForIDs(array( $Row["representation_id"]),array("preview")  );
     //var_export($m[$Row1["representation_id"]]['urls']);
     $p= $m[$Row1["representation_id"]]['urls'];
+    $style= $Row["access"]==1433?" border:thin solid green":(
+      $Row["access"]==1434?"border:thin solid yellow; opacity:0.9;":"border:thin solid red; opacity:0.5;"
+    );
 
-    echo "<img src='". $p['preview']."'>";
+    echo '<img style="'.$style.'" src="'. $p['preview'].'" >';
         
   
   }
