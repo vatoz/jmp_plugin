26fd0144ffd853152468cb2a4d0061d0474cd023
Vaclav Cerny <vasek@vasekcerny.cz>
2023-01-05 14:43:04 +0100

    Pochopená pole jsou hotová

--- views/victim.php
+++ views/victim.php
@@ -72,7 +72,7 @@ function fuzzyf($name,$endelement='type="text"' ){
     }
 
     $result.=
-    '
+    '</div><div> &nbsp;<br>
     <input '.$endelement .' id="'.$name.'_fuzzy_val" name="'.$name.'_fuzzy_val"  value="'.htmlspecialchars($candidate).'" >
 </div>
 
@@ -448,15 +448,17 @@ function loadAtt($id){
         'placedeparture'=> selplaces("SELECT  distinct place_id
         FROM `ca_places_x_occurrences`
         WHERE `type_id` = '69' 
-        AND occurrence_id in (SELECT occurrence_id FROM `ca_occurrences` WHERE `type_id` = '94')
+        AND occurrence_id in (SELECT occurrence_id FROM `ca_occurrences` WHERE `type_id` = '94' and (1=1)  )
         
         "),
+         
 
         'target'=> selplaces("SELECT  distinct place_id
         FROM `ca_places_x_occurrences`
         WHERE `type_id` = '70' 
-        AND occurrence_id in (SELECT occurrence_id FROM `ca_occurrences` WHERE `type_id` = '94')
+        AND occurrence_id in (SELECT occurrence_id FROM `ca_occurrences` WHERE `type_id` = '94' and (1=1) )
         "), 
+
         'fate'=>'SELECT item_id,name_singular
         FROM `ca_list_item_labels`
         WHERE `item_id` in (SELECT item_id
@@ -471,6 +473,16 @@ $f_text=array("fname","lname","lastplace","deathplace","remark", "reason","place
 $f_date=array("born","death","arrival","departure");
 
 
+
+foreach(array("tdt","tnv") as $key ){ // evaluate $keys_tdt,$keys_tnv
+    $resultname="keys_".$key; 
+    $$resultname=array();
+    $dotaz = $pdo->query($autocompletes[$key]);
+    foreach($dotaz as $Row){
+        $$resultname[]=$Row['occurrence_id'];
+    }
+    $$resultname=implode(",",$$resultname);
+}
 
 
 
@@ -519,44 +531,52 @@ $f_date=array("born","death","arrival","departure");
     
     
             row(
-                listf("tdt","Transport do Terezína", $autocompletes['tdt'],true)
+                listf("tdt","Transport do Terezína", $autocompletes['tdt'])
             ); //TODO nefunguje "vyplněný, a prázdný)
 
-            row("<div style='background-color:green;'>Řádky výše už fungují</div>", info("A občas i něco níže, co bylo podobné něčemu výše.","color:teal;"));
-
-
-
-
+            
             row(
                 txtf('trnr',"číslo v transportu"), fuzzyf('trnr')
             );
 
             row(
-                datef('arrival',"datum příjezdu")
+                datef('arrival',"datum příjezdu" ,"1941-11-01","1945-04-07")
             );
-
+            
 
             row(
-                listf('placedeparture',"místo odjezdu", $autocompletes['placedeparture'])
+                listf('placedeparture',"místo odjezdu",str_replace ("(1=1)","(occurrence_id in (".$keys_tdt. "))"  , $autocompletes['placedeparture']))
             );
         
             row(
-                listf("tnv","Transport na východ", $autocompletes['tnv'], true)
+                listf("tnv","Transport na východ", $autocompletes['tnv'])
             );
 
 
             row(
             
-                txtf('tnv_nr',"číslo v transportu"));
+                txtf('tnv_nr',"číslo v transportu")
+                , fuzzyf('tnv_nr')
+            );
             row(
-                datef('departure',"datum odjezdu")
+                datef('departure',"datum odjezdu","1941-10-01","1945-04-07")
             );
 
             row(
-                listf('departure2place',"místo odjezdu", $autocompletes['departure2place']),    listf('target',"Cíl", $autocompletes['target'])
+                listf('departure2place',"místo odjezdu",
+                str_replace ("(1=1)","(occurrence_id in (".$keys_tnv. "))" , $autocompletes['placedeparture'] ) 
+                )
+                
+            ,   listf('target',"Cíl", str_replace ("(1=1)","(occurrence_id in (".$keys_tnv. "))"  , $autocompletes['target'])
+            
+            )
             
             
             );
+            
+            row("<div style='background-color:green;'>Řádky výše už fungují</div>", info("A občas i něco níže, co bylo podobné něčemu výše.","color:teal;"));
+
+
         
     row(info("Osud, musím zjistit jak se ukládá, nebo co tím bylo myšleno","color:teal;"),
         listf('fate',"Osud", $autocompletes['fate'])
@@ -576,16 +596,17 @@ $f_date=array("born","death","arrival","departure");
 <?php 
 //TODO:
 row(
-     info("chybějící pole, todo","color:teal;"),
+ info("chybějící pole, todo","color:teal;"),
  info("chceme víc exportů?","color:teal;"),
  info("jen mající totožné/jen nemající totožné?","color:teal;"),
  info("u fuzzy inputů nevyplněné","color:teal;"),
- info("vyplnit rozsahy datumů","color:teal;"));
-
+ info("vyplnit rozsahy datumů","color:teal;")
+);
 
 ?>
 
 </form>
+<form> <input type=submit value="vymazat filtry"></form>
 
 <style type="text/css">
     @media print {
@@ -609,31 +630,27 @@ row(
         #leftNav{display:none;       
         }    
 
-        td{padding-left:5px;border:thin solid lightgray}
+        td{padding-left:5px;border:thin solid lightgray;padding-right:3px;}
         .jmp_info span{display:none;}
-        .jmp_info:hover span{display:block;position:absolute;border:thin solid gray;background-color:white;padding:10px 10px 10px 10px; }
+        .jmp_info:hover span{display:block;position:absolute;border:thin solid gray;background-color:white;padding:10px 10px 10px 10px; z-index: 100; }
         
-        thead tr  th {            
+        thead tr th {            
             top: 55px;
             position: sticky;
-            z-index: 9999;
+            z-index: 99;
             background-color:rgba(255,255,255,0.4);
+            cursor: pointer;
         }
+   
 
-
-    
+        td a{opacity: 0.2;}
+        td:hover a{opacity:1;}
 </style>
-
 
 <div id=result>
 
 <h1> Seznam obětí holocaustu</h1>
 <?php
-
-
-
-
-
 
 //read requests
 $values=array();
@@ -649,7 +666,7 @@ foreach($f_text as $field){
 }
 
 
-$f_date=array("born","death","arrival","departure","regnr");
+$f_date=array("born","death","arrival","departure","regnr", "trnr","tnv_nr");
 
 foreach($f_date as $field){
     foreach(array($field,  $field."_fuzzy",$field."_fuzzy_val") as $field2){
@@ -677,13 +694,57 @@ foreach(array(37=>"deathplace",170=>'lastplace') as $type_id=>$name ){
 }
 
 
-foreach(array("tdt","tnv") as $occ){
+foreach(array("trnr"=>"tdt","tnv_nr"=>"tnv") as $nrfiltr=>$occ){
     if($values[$occ]<>""){
+        $resultname="keys_".$occ;// evaluate $keys_tdt,$keys_tnv
         $limits[$occ]= '(ca_entities.entity_id '.($values[$occ]=='empty'?' NOT':'').' in (
             SELECT entity_id  FROM `ca_entities_x_occurrences` WHERE  1=1         '.
-            (intval($values[$occ])? ' and occurrence_id = '.intval($values[$occ]) :"" )
+            (intval($values[$occ])? ' and occurrence_id = '.intval($values[$occ]) :
+            ' and occurrence_id in('.$$resultname. ')')
             .' )  )';
-    }
+        }
+
+        if(intval($values[$nrfiltr])>0 ){
+            if(intval($values[$occ])>0){
+                $foc=$values[$occ];
+            }else{
+                $resultname='keys_'.$occ;
+                $foc=$$resultname; //e.g.  $keys_tdt,$keys_tnv
+            }
+
+            $SQL= 
+            '(ca_entities.entity_id in (select entity_id
+            from  
+                ca_entities_x_occurrences 
+            left join ca_attributes 
+                on ( ca_attributes.table_num = 22 and  ca_attributes.row_id = ca_entities_x_occurrences.relation_id and ca_attributes.element_id=302)
+            left join ca_attribute_values
+                on ca_attributes.attribute_id  = ca_attribute_values.attribute_id          WHERE     
+                occurrence_id in ('.$foc.") AND ";
+
+            switch ($values[$nrfiltr.'_fuzzy']){
+                case "exact":
+                    $SQL.="value_longtext1 = ".intval($values[$nrfiltr]);
+                    break; 
+                case "bigger":
+                    $SQL.="CAST(value_longtext1 as UNSIGNED)  > ".intval($values[$nrfiltr]);
+                    break; 
+                case "smaller":            
+                    $SQL.="CAST(value_longtext1 as UNSIGNED)  < ".intval($values[$nrfiltr]);
+                    break; 
+                case "interval":
+                    $SQL.="( CAST(value_longtext1 as UNSIGNED)  >= ".intval($values[$nrfiltr]) .
+                    " AND CAST(value_longtext1 as UNSIGNED)  <= ".intval($values[$nrfiltr."_fuzzy_val"]) .")";
+                    break; 
+    
+            }
+            $SQL.=" ) )";
+
+            $limits[$nrfiltr]=$SQL;
+
+
+        }
+    
 }
 
 
@@ -703,25 +764,6 @@ if($values['fate']<>""){ //toto nefunguje todo opravit
 
 
 
-$aaautocompletes=array(
-
-
-    'placedeparture'=> selplaces("SELECT  distinct place_id
-    FROM `ca_places_x_occurrences`
-    WHERE `type_id` = '69' 
-    AND occurrence_id in (SELECT occurrence_id FROM `ca_occurrences` WHERE `type_id` = '94')
-    
-    "),
-
-'target'=> selplaces("SELECT  distinct place_id
-FROM `ca_places_x_occurrences`
-WHERE `type_id` = '70' 
-AND occurrence_id in (SELECT occurrence_id FROM `ca_occurrences` WHERE `type_id` = '94')
-")
-    
-
-
-);//todo remove, mám to tu jen jako temp text
 
 foreach(array(102=>"born",103=>"death") as $item_id =>$event ) {
     if(ymd2decimal($values[$event])>0){
@@ -737,8 +779,8 @@ foreach(array(102=>"born",103=>"death") as $item_id =>$event ) {
                 $datepart="lft.value_decimal2 < ".ymd2decimal($values[$event]);
                 break; 
             case "interval":
-                $datepart="( lft.value_decimal2 <= ".ymd2decimal($values[$event]) . " AND ".
-                "lft.value_decimal >=  ".ymd2decimal($values[$event.'_fuzzy_val'] ). " )";
+                $datepart="( lft.value_decimal1 <= ".ymd2decimal($values[$event]) . " AND ".
+                "lft.value_decimal1 >=  ".ymd2decimal($values[$event.'_fuzzy_val'] ). " )";
                 break; 
 
         }
@@ -757,7 +799,47 @@ foreach(array(102=>"born",103=>"death") as $item_id =>$event ) {
     } 
     }
 
-    echo info(var_export($values,true));
+
+
+
+    foreach(array("tdt"=>"arrival","tnv"=>"departure") as $key=>$event ) { /* occurrence dates */
+        if(ymd2decimal($values[$event])>0){
+    
+            switch ($values[$event.'_fuzzy']){
+                case "exact":
+                    $datepart="lft.value_decimal1 = ".ymd2decimal($values[$event]);
+                    break; 
+                case "bigger":
+                    $datepart="lft.value_decimal1 > ".ymd2decimal($values[$event]);
+                    break; 
+                case "smaller":            
+                    $datepart="lft.value_decimal1 < ".ymd2decimal($values[$event]);
+                    break; 
+                case "interval":
+                    $datepart="( lft.value_decimal1 >= ".ymd2decimal($values[$event]) . " AND ".
+                    "lft.value_decimal1 <=  ".ymd2decimal($values[$event.'_fuzzy_val'] ). " )";
+                    break; 
+    
+            }
+            $resultname="keys_".$key; // evaluate $keys_tdt,$keys_tnv
+
+            $limits[$event]= '(ca_entities.entity_id in (
+    
+                select  entity_id from  ca_entities_x_occurrences
+                left join ca_attributes attr on (attr.row_id=occurrence_id  and attr.table_num=67 and attr.element_id=162)
+                left join  ca_attribute_values lft on (   lft.attribute_id =attr.attribute_id )
+                              
+                where '.$datepart.'   AND occurrence_id in('.$$resultname.')
+                
+    
+                )
+                )';
+        } 
+        }
+
+
+
+    
     foreach(array(4212=>"regnr") as $item_id =>$value ) {
         if(intval($values[$value])>0){
 
@@ -822,7 +904,41 @@ foreach(array(102=>"born",103=>"death") as $item_id =>$event ) {
 
     }
 
+    foreach(  [
+        ["placedeparture",69,$keys_tdt],
+        ["departure2place",69,$keys_tnv],
+        ["target",70,$keys_tnv]
+        ] as $params){
 
+        $key=$params[0]; $type_id=$params[1]; $occurrence_id=$params[2];
+
+        if($values[$key]<>""){
+            $limits[$key]=
+            '(ca_entities.entity_id '.($values[$key]=='NULL'?' NOT':'').' in (
+
+            SELECT entity_id from ca_entities_x_occurrences where
+
+            occurrence_id in (
+
+                select occurrence_id from ca_places_x_occurrences where 
+                type_id= '.$type_id.'  AND '.
+                (intval($values[$key])? ' place_id = '.intval($values[$key])    : ''
+                ) .
+                ' AND occurrence_id in('.$occurrence_id.')'
+                .
+                '      )
+            )
+            )
+            ';
+
+
+        }
+
+
+
+
+    }
+    
 
 
 //execute query
@@ -845,11 +961,11 @@ implode( $values['onlyone']?" OR \n":" AND \n",$limits)
 .") order by displayname";
 //echo info($SQL);
 //echo info($values['onlyone']==true?" OR \n":" AND \n");
-
+/*
 foreach($limits as $limit ){
     echo info ("Použit filtr:<br><pre>".$limit."</pre>","color:red;");//todo odebrat
 
-}
+}*/
     //echo $SQL;
 try{    
 $dotaz = $pdo->query($SQL);
@@ -890,14 +1006,14 @@ $data2=loadOcc($Row['entity_id']);
 
 
     echo '<tr>';
-    echo '<td>'.$Row['surname'].'</td>';
+    echo '<td>'.$Row['surname'].' <a href="/index.php/editor/entities/EntityEditor/Edit/entity_id/'.$Row['entity_id'].'" class="no-print"  title="Upravit">e</a> </td>';
     echo '<td>'.$Row['forename'].'</td>';
     echo '<td>'.$Row['prefix'].'</td>';
     echo '<td>'.$data['born'] .'</td>';
     echo '<td>'.$data['lastplace'] .'</td>';
 
     echo '<td>'.$data2['t']['code'].'</td>';
-    echo '<td>'.$data2['t']['tnum'].'</td>';
+    echo '<td>'.$data2['t']['tnum'].'</td>'; 
     echo '<td>'.$data2['t']['start'].'</td>';
     echo '<td>'.$data2['t']['date'].'</td>';
 
@@ -930,50 +1046,21 @@ echo "<hr>Celkem ve výpisu: ".$cnt;
 </div>
 
 <script>
-$( function() {
-    //$.datepicker( $.datepicker.regional[ "cs_CZ" ] );
-<?php
 
-
-    foreach ($autocompletes as $item=>$SQL){
-        $dotaz = $pdo->query($SQL);
-        $lastpl=array();
-        foreach($dotaz    as $Row){
-            $lastpl[]=$Row['name'];
-        }
-       // echo "var TAG".$item." = ".json_encode($lastpl).";\n";
-       // echo '$( "#'.$item.'" ).autocomplete({source: TAG'.$item.'});'."\n";
-       // echo '$( "#'.$item.'" ).css("border","thick solid green");'."\n";
+//https://stackoverflow.com/questions/3160277/jquery-table-sort
+  $('th').click(function(){
+    var table = $(this).parents('table').eq(0)
+    var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+    this.asc = !this.asc
+    if (!this.asc){rows = rows.reverse()}
+    for (var i = 0; i < rows.length; i++){table.append(rows[i])}
+})
+function comparer(index) {
+    return function(a, b) {
+        var valA = getCellValue(a, index), valB = getCellValue(b, index)
+        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
     }
-
-    foreach($f_date  as $d2){
-        foreach(array($d2,$d2.'_fuzzy_val') as $date){
-         /*   echo '$( "#'.$date.'" ).datepicker();'."\n";
-            echo '$( "#'.$date.'"  ).datepicker( "option", "dateFormat", "yy-mm-dd");'."\n";
-            echo '$( "#'.$date.'"  ).datepicker( "option", "regional", "cs");'."\n";
-            echo '$( "#'.$date.'"  ).datepicker( "option", "changeYear", true);'."\n";
-            echo '$( "#'.$date.'"  ).datepicker( "option", "changeMonth", true);'."\n";
-            if($d2=="born"){
-                echo '$( "#'.$date.'"  ).datepicker( "option", "yearRange", "1800:1945");'."\n"; 
-            }else{
-                echo '$( "#'.$date.'"  ).datepicker( "option", "yearRange", "1930:1945");'."\n"; 
-            }
-           */ 
-
-              
-            $candidate="";
-            if(isset($_REQUEST[$d2])){
-                $candidate=$_REQUEST[$d2];
-                if(preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$candidate)){
-                   // echo '$( "#'.$date.'"  ).datepicker(  "setDate", "'.$candidate.'");'."\n";
-                }                
-            }
-        }
-    }
-
-
-
-    ?>    
-  } );
+}
+function getCellValue(row, index){ return $(row).children('td').eq(index).text() }
 
   </script>
